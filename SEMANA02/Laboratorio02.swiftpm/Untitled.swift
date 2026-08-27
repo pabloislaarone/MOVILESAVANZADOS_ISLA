d79fd7a03df576991dca3d2e2fea5bc62bd85300
pabloislaarone <pablo.isla@tecsup.edu.pe>
2026-08-27 12:22:20 -0500

    ejemplo

--- SEMANA02/Laboratorio02.swiftpm/Untitled.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-
-print ("Nùmero 1")
-let a = Double (readLine() ?? "") ?? 0
-print ("Nùmero 2")
-let b = Double (readLine() ?? "") ?? 0
-
-let suma = a + b
-print ("La suma es \(suma)")
