e07f7ef8b6b82ac20e57f14ee91fe034db39d5ad
pabloislaarone <pablo.isla@tecsup.edu.pe>
2026-08-27 12:08:53 -0500

    Ejemplo movil

---
+++ SEMANA02/Laboratorio02.swiftpm/Untitled.swift
@@ -0,0 +1,9 @@
+import Foundation
+
+print ("Nùmero 1")
+let a = Double (readLine() ?? "") ?? 0
+print ("Nùmero 2")
+let b = Double (readLine() ?? "") ?? 0
+
+let suma = a + b
+print ("La suma es \(suma)")
